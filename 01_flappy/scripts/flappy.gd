ced0dfa57ff42ac87a492ab9c8e546a22b6e9093
Evan <evan.c.ohme@gmail.com>
2023-12-15 02:39:44 -0700

    Add readme file, small changes in flappy script

--- 01_flappy/scripts/flappy.gd
+++ 01_flappy/scripts/flappy.gd
@@ -5,23 +5,24 @@ extends CharacterBody2D
 @export var flap_velocity: float = -400.0
 
 
-func _process(_delta: float) -> void:
-	pass
-
-
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		
-	# Handle rotation.
+	velocity.y += gravity * delta
+	
+	handle_rotation()
+	handle_input()
+	
+	move_and_slide()
+
+
+func handle_input() -> void:
+	# Flap
+	if Input.is_action_just_pressed("flap"):
+		velocity.y = flap_velocity
+
+
+func handle_rotation() -> void:
 	var flap_lerp: float = lerpf(get_rotation_degrees(), (velocity.y * 0.5) - 100.0, 0.05)
 	var flap_rotation: float = clampf(flap_lerp, -35.0, 85.0)
 	set_rotation_degrees(flap_rotation)
-	
-	# Handle Flap.
-	if Input.is_action_just_pressed("flap"):
-		velocity.y = flap_velocity
-	
-	move_and_slide()
 
