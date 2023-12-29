959aee9eef64fb654b6b338b7831c913d05533a4
evan <evan.c.ohme@gmail.com>
2023-12-28 20:53:50 -0700

    Fix ground sprite to tile and not flicker

---
+++ 01_flappy/scripts/ground.gd
@@ -0,0 +1,29 @@
+extends StaticBody2D
+
+
+@export var scroll_speed: float = 5.7
+
+
+var floor1: Sprite2D
+var floor2: Sprite2D
+
+var start_pos: float = 792.0
+var end_pos: float = -start_pos
+
+
+func _ready() -> void:
+	floor1 = $Floor1
+	floor2 = $Floor2
+
+
+func _physics_process(_delta: float) -> void:
+	# Floor1
+	floor1.position.x -= scroll_speed
+	if floor1.position.x <= end_pos:
+		floor1.position.x = start_pos
+	
+	# Floor2
+	floor2.position.x -= scroll_speed
+	if floor2.position.x <= end_pos:
+		floor2.position.x = start_pos
+
